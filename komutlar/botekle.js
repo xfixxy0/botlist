678e50de45a816fc25873253772c88e0e86bdde9
xfixxy0 <73495457+xfixxy0@users.noreply.github.com>
2020-12-22 13:49:30 +0300

    Update botekle.js

--- komutlar/botekle.js
+++ komutlar/botekle.js
@@ -23,27 +23,27 @@ if (message.channel.id !=kinal) return message.channel.send(`Bot ekleme kodunu S
       .then(msg => msg.delete(2500));
   let kanal = await db.fetch(`hgK_${message.guild.id}`); 
   if (!kanal) return;
-  const embed = new Discord.RichEmbed()
+  const embed = new Discord.MessageEmbed()
     .setColor("GREEN")
     .setDescription(`Botun başarıyla başvuruya alındı.Yapman gereken sabırla onaylanmasını/reddedilmesini beklemek.`);
      message.author.send(embed);
-  const embed3 = new Discord.RichEmbed()
+  const embed3 = new Discord.MessageEmbed()
     .setColor("GREEN")
     .setDescription(`${message.author}, botun başvuruya eklendi.`);
      message.channel.send(embed3)
     .then(msg => msg.delete(2500));
-  const embed2 = new Discord.RichEmbed()
+  const embed2 = new Discord.MessageEmbed()
   .setColor("BLUE")
   .setDescription(
     `:white_circle: | ${message.author} adlı kullanıcı prefixi **\`${prefix}\`** olan <@${botid}> adlı botu ile başvuru yaptı!`);
-  client.channels.get(kanal).send(embed2); // Kanal ID
+  client.channels.cache.get(kanal).send(embed2); // Kanal ID
   let yetkilikanal = await db.fetch(`hgK2_${message.guild.id}`); 
   if (!yetkilikanal) return;
-  const yetkili = new Discord.RichEmbed()
+  const yetkili = new Discord.MessageEmbed()
   .setColor("BLUE")
   .setTitle("Başvuru")
   .setDescription(`**Bot Sahibi**:\n${message.author}\n**Bot ID**:\n${botid}\n**Bot Prefix**:\n${prefix}\n**Davet Linkleri**:\n[Perm 0](https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=0) | [Perm 8](https://discord.com/oauth2/authorize?client_id=${botid}&scope=bot&permissions=8)`)
-  client.channels.get(yetkilikanal).send(yetkili);
+  client.channels.cache.get(yetkilikanal).send(yetkili);
 };
 exports.conf = {
   enabled: true,
